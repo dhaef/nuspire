0018087f5221e3c0767c81825a85158b31d130bb
dhaef <derekhaefner@gmail.com>
2021-01-23 16:20:04 -0500

    add cognito config variables

---
+++ src/utils/amplify.js
@@ -0,0 +1,4 @@
+export const UserPoolId = 'us-east-2_xi7hLKpHp'
+export const ClientId = '3oipmen0d58bnok6fm5igprqov'
+export const region = 'eu-west-1'
+export const IdentityPoolId = 'eu-west-1:a1564b96-eeef-4f38-84e1-33a0d2f50ab6'
